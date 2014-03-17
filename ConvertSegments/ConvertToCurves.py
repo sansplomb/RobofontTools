3632db86252afc15f07fd743ba3d0fa43797dd31
sansplomb <jeremie@hornus.com>
2014-03-17 22:23:15 +0100

    New version for ConvertSegments
    - simplification of ConvertToLines.
    - re-write of ConvertToCurves, still small bugs with open contours

--- ConvertSegments/ConvertToCurves.py
+++ ConvertSegments/ConvertToCurves.py
@@ -1,43 +1,57 @@
-		
+def computeInsertAttributes():
+	g = CurrentGlyph()
+	# SAM SAYS: j'ai viré x_list et y_list car non utilisés
+	insertAttributes = []
+	for c_index in range(len(g.contours)):
+		c = g.contours[c_index]
+		for s_index in range(len(c.segments)):
+			s = c.segments[s_index]
+			print s.type
+			if not s.selected:
+			    continue
+			if s.type == "line":
+				endPoint = s[0]
+				prev = c.segments[s_index - 1]
+				if prev.type == 'curve':
+					startPoint = prev[2]
+				elif prev.type in ['line', 'move']:
+					startPoint = prev[0]			    
+				else: # SAM SAYS: ai ajouté ce warning:
+					print("WARNING: I don't know this kind of segment: "+prev.type)        
+					continue
+				
+				third_x = int((endPoint.x - startPoint.x) / 3)
+				third_y = int((endPoint.y - startPoint.y) / 3)
+				
+				insertAttributes.append(
+						(	c_index,
+							s_index,
+							(startPoint.x + third_x, startPoint.y + third_y),
+							(endPoint.x   - third_x, endPoint.y   - third_y),
+							(endPoint.x, endPoint.y)
+						)
+					)
+	return insertAttributes
+
 def convertToCurve():
-	g = CurrentGlyph()	
-	if g and g.selection != []:
-		g.prepareUndo()
-		a_list_x = []
-		a_list_y = []
-		insertAttributes = []
-		
-		for c_index in range(len(g.contours)):
-			c = g.contours[c_index]
-			for s_index in range(len(c.segments)):
-				s = c.segments[s_index]
-				if s.selected and s.type == "line":
-					endPoint = s[0]
-					if c.segments[s_index - 1].type == 'curve':
-						startPoint = c.segments[s_index - 1][2]
-					elif c.segments[s_index - 1].type == 'line':
-						startPoint = c.segments[s_index - 1][0]
-					#startPoint = c.segments[s_index - 1][0]
-					#print startPoint, endPoint 
-					
-					a_list_x = [startPoint.x, endPoint.x]
-					a_list_y = [startPoint.y, endPoint.y]
-					third_x = int((endPoint.x - startPoint.x) / 3)
-					third_y = int((endPoint.y - startPoint.y) / 3)
-					
-					insertAttributes.append([c_index, s_index, (startPoint.x + third_x, startPoint.y + third_y), (endPoint.x - third_x, endPoint.y - third_y), (endPoint.x, endPoint.y)])
-					
-		for i in insertAttributes:
-			g.contours[i[0]].insertSegment(i[1], 'curve', (i[2], i[3], i[4]), False)
-			#print i[1], len(g.contours[i[0]].segments)
-			if i[1] == 0:
-				#print 'first segment'
-				g.contours[i[0]].removeSegment(1)
-			elif i[1] == len(g.contours[i[0]].segments) - 2:
-				#print 'last segment'
-				g.contours[i[0]].removeSegment(0)
-			else:
-				g.contours[i[0]].removeSegment(i[1]+1)
-		g.update()
+	g = CurrentGlyph()
+	# SAM SAYS: je test la négation et 'return', ça permet de ne pas avoir à indenter ce qui suit.
+	# (compare avec ton test initial)
+	if (not g) or g.selection == []:
+		return
+	g.prepareUndo()
+	for i in computeInsertAttributes():
+		# SAM SAYS: j'ai changé 'i' en un 'tuple' (en l'occurence, un quintuplet) plutôt qu'une liste,
+		# et j'extrais les cinque valeurs d'un coup:
+		c_idx, s_idx, p1, p2, p3 = i
+		g.contours[c_idx].insertSegment(s_idx, 'curve', (p1, p2, p3), False)
+		#print s_idx, len(g.contours[c_idx].segments)
+		# SAM SAYS: pas besoin du cas s_idx == 0. Par contre, je pense que ci-dessous, il faut '- 1' et pas '- 2'
+		if s_idx == len(g.contours[c_idx].segments) - 2:
+			#print 'last segment'
+			g.contours[c_idx].removeSegment(0)
+		else:
+			g.contours[c_idx].removeSegment(s_idx+1)
+	g.update()
 
 convertToCurve()
--- ConvertSegments/ConvertToLines.py
+++ ConvertSegments/ConvertToLines.py
@@ -13,70 +13,71 @@ def convertToLine():
 			for s_index in range(len(c.segments)):
 				s = c.segments[s_index]
 				if s.selected and s.type == "curve":
-					#print 'curve'
-					endPoint = c.segments[s_index][2]
-					if c.segments[s_index - 1].type == 'curve':
-						startPoint = c.segments[s_index - 1][2]
-					elif c.segments[s_index - 1].type == 'line':
-						startPoint = c.segments[s_index - 1][0]
-					#print startPoint, endPoint 
+				    s.type = "line"
+		#			#print 'curve'
+		#			endPoint = c.segments[s_index][2]
+		#			if c.segments[s_index - 1].type == 'curve':
+		#				startPoint = c.segments[s_index - 1][2]
+		#			elif c.segments[s_index - 1].type == 'line':
+		#				startPoint = c.segments[s_index - 1][0]
+		#			#print startPoint, endPoint 
 					
-					insertAttributes.append([c_index, s_index, (endPoint.x, endPoint.y)])
+		#			insertAttributes.append([c_index, s_index, (endPoint.x, endPoint.y)])
 					
-		for i in insertAttributes:
-			if i[1] == 0:
-				#print 'first segment'
-				if g.contours[i[0]][1].type == 'curve':
-					#print 'next is curve'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
-					g.contours[i[0]].removeSegment(i[1]+1)
-				elif g.contours[i[0]][1].type == 'line':
-					#print 'next is line'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#for i in insertAttributes:
+		#	if i[1] == 0:
+		#		#print 'first segment'
+		#		if g.contours[i[0]][1].type == 'curve':
+		#			#print 'next is curve'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#			g.contours[i[0]].removeSegment(i[1]+1)
+		#		elif g.contours[i[0]][1].type == 'line':
+		#			#print 'next is line'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
 					
-					g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#			g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][i[1] + 2].points[0].y
 
-					g.contours[i[0]].removeSegment(i[1]+1)
-			elif i[1] == len(g.contours[i[0]].segments) - 1:
-				#print 'last segment'
-				if g.contours[i[0]][0].type == 'curve':
-					#print 'next is curve'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][0].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][0].points[0].y
-					g.contours[i[0]].removeSegment(i[1]+1)
-				elif g.contours[i[0]][0].type == 'line':
-					#print 'next is line'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][0].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][0].points[0].y
+		#			g.contours[i[0]].removeSegment(i[1]+1)
+		#	elif i[1] == len(g.contours[i[0]].segments) - 1:
+		#		#print 'last segment'
+		#		if g.contours[i[0]][0].type == 'curve':
+		#			#print 'next is curve'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][0].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][0].points[0].y
+		#			g.contours[i[0]].removeSegment(i[1]+1)
+		#		elif g.contours[i[0]][0].type == 'line':
+		#			#print 'next is line'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][0].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][0].points[0].y
 					
-					g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][0].points[0].x
-					g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][0].points[0].y
+		#			g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][0].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][0].points[0].y
 
-					g.contours[i[0]].removeSegment(i[1]+1)
-			else:
-				if g.contours[i[0]][i[1]+1].type == 'curve':
-					#print 'next is curve'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
-					g.contours[i[0]].removeSegment(i[1]+1)
-				if g.contours[i[0]][i[1]+1].type == 'line':
-					#print 'next is line'
-					g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
-					g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#			g.contours[i[0]].removeSegment(i[1]+1)
+		#	else:
+		#		if g.contours[i[0]][i[1]+1].type == 'curve':
+		#			#print 'next is curve'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#			g.contours[i[0]].removeSegment(i[1]+1)
+		#		if g.contours[i[0]][i[1]+1].type == 'line':
+		#			#print 'next is line'
+		#			g.contours[i[0]].insertSegment(i[1], 'line', [i[2]], False)
+		#			g.contours[i[0]][i[1]+1].points[0].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[0].y = g.contours[i[0]][i[1] + 2].points[0].y
 					
-					g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][i[1] + 2].points[0].x
-					g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][i[1] + 2].points[0].y
+		#			g.contours[i[0]][i[1]+1].points[1].x = g.contours[i[0]][i[1] + 2].points[0].x
+		#			g.contours[i[0]][i[1]+1].points[1].y = g.contours[i[0]][i[1] + 2].points[0].y
 
-					g.contours[i[0]].removeSegment(i[1]+1)
+		#			g.contours[i[0]].removeSegment(i[1]+1)
 			
 		g.update()
 
