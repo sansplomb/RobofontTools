c44ede935dd9e6ad7463436f9459a07dd070b1a2
sansplomb <jeremie@hornus.com>
2014-03-19 09:04:08 +0100

    Assign font Layer from another font
    Copy glyphs from a Target font to a Source font’s new custom layer

---
+++ AssignFontLayer/AssignFontLayer.py
@@ -0,0 +1,103 @@
+from vanilla import *
+from robofab.pens.digestPen import *
+import string
+
+
+af = AllFonts()
+f = CurrentFont()
+fontList = []
+
+for f in af:
+	fontList.append(f.info.familyName + " " + f.info.styleName)
+		
+class AssignFontLayerWindow(object):
+	
+	def __init__(self):
+		self.w = Window((200, 300), "Assign Font Layer")
+		self.w.textBoxSource = TextBox((10, 10, -10, 30), "Source Font:")
+		self.w.popUpButtonSource = PopUpButton((10, 30, -10, 30), fontList, callback=self.popUpButtonSourceCallback, sizeStyle = "regular")
+		
+		self.w.textBoxTarget = TextBox((10, 70, -10, 30), "Target Font:")
+		self.w.popUpButtonTarget = PopUpButton((10, 90, -10, 30), fontList, callback=self.popUpButtonTargetCallback, sizeStyle = "regular")
+		
+		self.w.textBox = TextBox((10, 130, -10, 30), "Layer Name in Target Font:")
+		self.w.textEditor = TextEditor((10, 160, -10, 30),
+							callback=self.textEditorCallback)
+							
+							
+		self.fontSourceName = fontList[0]
+		self.fontTargetName = fontList[0]
+		self.fontSourceIndex = 0
+		self.fontTargetIndex = 0
+		
+		self.fontSourceList = self.w.popUpButtonSource.getItems()
+		self.fontTargetList = self.w.popUpButtonTarget.getItems()
+		
+		self.w.buttonOK = Button((10, -30, 90, -10), "OK",
+							callback=self.buttonOKCallback)
+		self.w.buttonCancel = Button((110, -30, -10, -10), "Cancel",
+							callback=self.buttonCancelCallback)
+		self.layerName = "untitled_layer"
+		self.w.textEditor.set(self.layerName)
+		self.w.center()
+		self.w.open()
+		
+	def buttonOKCallback(self, sender):
+		 #print "Ok"
+		 
+		sourceFont = af[self.fontSourceIndex]
+		targetFont = af[self.fontTargetIndex]
+		#print "Source Font:", sourceFont
+		#print "Target Font:", targetFont
+		#print "New Layer Name:", self.layerName
+		 
+		 #get the first glyph's name
+		firstGlyphName = targetFont.keys()[0]
+		 #create a new layer with specified layerName
+		targetFont[firstGlyphName].getLayer(self.layerName)
+		 #parse the target font's glyphs
+		for gT in targetFont:
+		 	#parse the source font's glyphs
+			for gS in sourceFont:
+		 		#if their names match, I want the targetLayer to be a copy of the sourceLayer
+				if gT.name == gS.name:
+					sourceLayer = gS.getLayer("foreground")
+		 			targetLayer = gT.getLayer(self.layerName)
+		 			#clear contours if any
+		 			targetLayer.clearContours()
+		 			gT.flipLayers("foreground", self.layerName)
+					targetFont[gT.name] = gS.copy()
+					gT.flipLayers("foreground", self.layerName)
+		self.w.close()
+		 
+	def buttonCancelCallback(self, sender):
+		 #print "Cancel"
+		 self.w.close()
+		
+	def popUpButtonSourceCallback(self, sender):
+		#print "pop up button selection!", sender.get()
+		
+		self.fontSourceIndex = sender.get()
+		self.fontSourceName = self.fontSourceList[sender.get()]
+		#print self.fontName
+		
+	def popUpButtonTargetCallback(self, sender):
+		#print "pop up button selection!", sender.get()
+		
+		self.fontTargetIndex = sender.get()
+		self.fontTargetName = self.fontTargetList[sender.get()]
+		#print self.fontName
+		
+	def textEditorCallback(self, sender):
+
+		#layerName = sender.get().title()
+		layerName = ''
+		layerNameList = sender.get().split(" ")
+		for i in range(len(layerNameList)):
+			layerName += layerNameList[i]
+			if i < len(layerNameList)-1:
+				layerName += '_'
+		self.layerName = layerName
+
+
+AssignFontLayerWindow()
